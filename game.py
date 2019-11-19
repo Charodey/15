bf8d67d77196a95c45920e492a58c08ff50fb5e9
Char <charjob@gmail.com>
2019-11-19 23:21:32 +0500

    apply PEP8

--- game.py
+++ game.py
@@ -1,95 +1,100 @@
-import sys, pygame, random
+import sys
+import random
+
+import pygame
+
 
 def check_board(board):
-  last = []
-  sum = 0
-  pos = -1
-  for point in board:
-    pos = pos + 1
-    if point == 16:
-      sum = sum + int(pos / 4) + 1
-    else:
-      last.append(point)
-      for p in range(1, point):
-        if p not in last:
-          sum = sum + 1
+    '''Проверяем, что сгенерированный набор фишек - решаем'''
+    last = []
+    total = 0
+    pos = -1
+    for point in board:
+        pos = pos + 1
+        if point == 16:
+            total = total + int(pos / 4) + 1
+        else:
+            last.append(point)
+            for p in range(1, point):
+                if p not in last:
+                    total = total + 1
 
-  return sum % 2 == 0
+    return total % 2 == 0
 
 def generate():
-  board = list(range(1, 17))
-  random.shuffle(board)
-  return board if check_board(board) else generate()
+    '''Генерация фишек'''
+    board = list(range(1, 17))
+    random.shuffle(board)
+    return board if check_board(board) else generate()
 
 def render():
-  screen.fill(black)
+    screen.fill(BLACK)
 
-  pos = -1
-  for n in board:
-    pos = pos + 1
-    j = int(pos / 4)
-    i = pos % 4
+    pos = -1
+    for n in board:
+        pos = pos + 1
+        j = int(pos / 4)
+        i = pos % 4
 
-    if n == 16:
-      continue
+        if n == 16:
+            continue
 
-    x = rectSize * i + borderWidth
-    y = rectSize * j + borderWidth
-    width = rectSize - borderWidth * (2 if i == 3 else 1)
-    height = rectSize - borderWidth * (2 if j == 3 else 1)
-    
-    rect = pygame.Rect(x, y, width, height)
-    pygame.draw.rect(screen, [136,136,136], rect)
+        x = RECT_SIZE * i + BORDER_WIDTH
+        y = RECT_SIZE * j + BORDER_WIDTH
+        width = RECT_SIZE - BORDER_WIDTH * (2 if i == 3 else 1)
+        height = RECT_SIZE - BORDER_WIDTH * (2 if j == 3 else 1)
 
-    label = myfont.render(str(n), 1, (220, 220, 220))
-    screen.blit(label, (x + width/2 - label.get_width()/2, y + height/2 - label.get_height()/2))
+        rect = pygame.Rect(x, y, width, height)
+        pygame.draw.rect(screen, [136, 136, 136], rect)
 
-  pygame.display.flip()
+        label = MY_FONT.render(str(n), 1, (220, 220, 220))
+        screen.blit(label, (x + width/2 - label.get_width()/2, y + height/2 - label.get_height()/2))
+
+    pygame.display.flip()
 
 def check_win():
-  for i in range(1, 17):
-    if i != board[i-1]:
-      break
-    if i == 16:
-      label = pygame.font.SysFont('monospace', 82).render('WIN', 1, (220, 10, 10))
-      screen.blit(label, (width/2 - label.get_width()/2, height/2 - label.get_height()/2))
-      pygame.display.flip()
+    for i in range(1, 17):
+        if i != board[i-1]:
+            break
+        if i == 16:
+            label = pygame.font.SysFont('monospace', 82).render('WIN', 1, (220, 10, 10))
+            screen.blit(label, (width/2 - label.get_width()/2, height/2 - label.get_height()/2))
+            pygame.display.flip()
+
 
 if __name__ == '__main__':
 
-  pygame.init()
-  pygame.display.set_caption("Пятнашки")
+    pygame.init()
+    pygame.display.set_caption("Пятнашки")
 
-  myfont = pygame.font.SysFont("monospace", 48)
+    MY_FONT = pygame.font.SysFont("monospace", 48)
 
-  size = width, height = 400, 400
-  speed = [-1, 1]
-  black = 0, 0, 0
-  borderWidth = 5
-  rectSize = width * .25
+    SIZE = width, height = 400, 400
+    BLACK = 0, 0, 0
+    BORDER_WIDTH = 5
+    RECT_SIZE = width * .25
 
-  screen = pygame.display.set_mode(size)
-  
-  board = generate()
-  render()
+    screen = pygame.display.set_mode(SIZE)
 
-  while 1:
-      
-      for event in pygame.event.get():
-          if event.type == pygame.QUIT: sys.exit()
-      
-      isCLicked, *q = pygame.mouse.get_pressed()
+    board = generate()
+    render()
 
-      if isCLicked:
-        x, y = pygame.mouse.get_pos()
-        i = int(x / rectSize)
-        j = int(y / rectSize)
-        pos = j*4+i
-        n = board[pos]
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
 
-        if n != 16:
-          emptyPos = board.index(16)
-          if emptyPos in [pos-1, pos+1, pos+4, pos-4]:
-            board[emptyPos], board[pos] = board[pos], board[emptyPos]
-            render()
-            check_win()
+        is_clicked, *q = pygame.mouse.get_pressed()
+
+        if is_clicked:
+            x, y = pygame.mouse.get_pos()
+            i = int(x / RECT_SIZE)
+            j = int(y / RECT_SIZE)
+            pos = j*4+i
+            n = board[pos]
+
+            if n != 16:
+                empty_pos = board.index(16)
+                if empty_pos in [pos-1, pos+1, pos+4, pos-4]:
+                    board[empty_pos], board[pos] = board[pos], board[empty_pos]
+                    render()
+                    check_win()
--- test_game.py
+++ test_game.py
@@ -1,27 +1,38 @@
-import unittest, random
+import unittest
+import random
+
 from game import check_board
+
 
 class TestGame(unittest.TestCase):
 
-  def test_check_board(self):
-    i = 0
-    cnt = 1000
-    cnt_true = 0
-    board = list(range(1, 17))
-    while (i<1000):
-      random.shuffle(board)
-      cnt_true = cnt_true + (1 if check_board(board) else 0)
-      i = i + 1
-    
-    cnt_true_percent = cnt_true / cnt * 100
-    self.assertTrue(45 < cnt_true_percent < 55)
+    def test_check_board(self):
+        '''Число решаемых наборов фишек при случайной генерации должно составлять ~50%'''
+        i = 0
+        cnt = 1000
+        cnt_true = 0
+        board = list(range(1, 17))
+        while i < 1000:
+            random.shuffle(board)
+            cnt_true = cnt_true + (1 if check_board(board) else 0)
+            i = i + 1
 
-  def test_check_board_predefined(self):
-    board = list(range(1, 17))
-    self.assertTrue(check_board(board))
-    board[14] = 14
-    board[13] = 15
-    self.assertFalse(check_board(board))
+        cnt_true_percent = cnt_true / cnt * 100
+        self.assertTrue(45 < cnt_true_percent < 55)
+
+    def test_check_board_predefined(self):
+        '''Проверка решаемости комбинаций
+
+        Если у решаемой комбинации фишек поменять местами 2 последовательных элемента,
+        то комбинация станет нерешаемой
+
+        '''
+        board = list(range(1, 17))
+        self.assertTrue(check_board(board))
+        board[14] = 14
+        board[13] = 15
+        self.assertFalse(check_board(board))
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
