b3a30dad200853cd62d9862734592acf1ab0ebc3
Char <charjob@gmail.com>
2019-11-24 17:41:58 +0500

    fix test for method 'is_solvable_board'

--- test_game.py
+++ test_game.py
@@ -6,36 +6,40 @@ from game import Game15
 
 class TestGame(unittest.TestCase):
 
-    def test_check_board(self):
+    def test_is_solvable_board(self):
         """Число решаемых наборов фишек при случайной генерации должно составлять ~50%"""
-        game = Game15()
+        for side_size in range(2, 9):
+            game = Game15(side_size)
 
-        i = 0
-        cnt = 1000
-        cnt_true = 0
-        board = list(range(1, 17))
-        while i < 1000:
-            random.shuffle(board)
-            cnt_true = cnt_true + (1 if game.check_board(board) else 0)
-            i = i + 1
+            i = 0
+            cnt = 1000
+            cnt_true = 0
+            board = list(range(1, side_size ** 2 + 1))
+            while i < 1000:
+                random.shuffle(board)
+                cnt_true = cnt_true + (1 if game.is_solvable_board(board) else 0)
+                i = i + 1
 
-        cnt_true_percent = cnt_true / cnt * 100
-        self.assertTrue(45 < cnt_true_percent < 55)
+            cnt_true_percent = cnt_true / cnt * 100
+            self.assertTrue(45 < cnt_true_percent < 55)
 
-    def test_check_board_predefined(self):
+    def test_is_solvable_board_predefined(self):
         """Проверка решаемости комбинаций
 
         Если у решаемой комбинации фишек поменять местами 2 последовательных элемента,
         то комбинация станет нерешаемой
 
         """
-        game = Game15()
+        for side_size in range(2, 11):
+            board_count = side_size ** 2
 
-        board = list(range(1, 17))
-        self.assertTrue(game.check_board(board))
-        board[14] = 14
-        board[13] = 15
-        self.assertFalse(game.check_board(board))
+            game = Game15(side_size)
+            # решаемая комбинация
+            board = list(range(1, board_count + 1))
+            self.assertTrue(game.is_solvable_board(board))
+            # нерешаемая комбинация
+            board[board_count - 3], board[board_count - 2] = board[board_count - 2], board[board_count - 3]
+            self.assertFalse(game.is_solvable_board(board))
 
 
 if __name__ == '__main__':
